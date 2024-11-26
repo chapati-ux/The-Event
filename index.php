5e6dd2902c1a983c38332f074dc8a653229e1f94
jayesh pujari <jpujari575@gmail.com>
2024-11-27 02:27:06 +0530

    code added

--- index.php
+++ index.php
@@ -4,8 +4,148 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link
+    href="https://cdn.jsdelivr.net/npm/remixicon@4.5.0/fonts/remixicon.css"
+    rel="stylesheet"/>
+    <link
+       rel="stylesheet"
+       href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"
+     />
+
 </head>
 <body>
+    <div id="whatsapp"></div>
+    <div id="main">
+       <nav>
+        <div id="logo"></div>
+        <div id="links">
+            <ul>
+               <a href="">Home</a>
+               <a href="">About Us</a>
+               <a href="">Gallery</a>
+               <a href="">Services</a>
+               <a href="">Events</a>
+               <a href="">Blogs</a>
+               <a href="">Contact Us</a>
+               <button><b>Coming Soon</b></button>
+               <i class="ri-menu-line"></i>
+            </ul>
+        </div>
+
+        <div id="slider">
+       <div id="links1">
+            <ul>
+               <li>Home</li>
+               <li>About Us</li>
+               <li>Gallery</li>
+               <li>Services</li>
+               <li>Events</li>
+               <li>Blogs</li>
+               <li>Contact Us</li>
+               <button>Coming Soon</button>
+            </ul>
+        </div>
+       </div>
+       </nav>
+     
+
+       <!-- page1-->
+      <div id="page1">
+        <div id="textcontent">
+            <div id="area">
+                <h1>Turning Moments into <br>
+                <span>Memories</span> </h1>
+               
+                <p>Tired of complecated registrations? <br>
+                try The Happening Events - easy Sign-ups for events!</p>
+
+               <button><a href="">Start planning Your Event</a></button> 
+            </div>
+        </div>
+        <div id="image">
+            <img src="https://s3-alpha-sig.figma.com/img/9cd1/4358/070984eec1311eee22c33409562968b0?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mBTuIFBdhOx6R~6rHYuFkNuBDiV-AbgV7T1EJXZizqY9WT6K4vxxMLWpR3xw4waQlIYz0brFzIrN59291vhoF8Dk-OpKvLiBtvmr47ptAuNL5JZZwD1MbKlALIvS5~9ItRcmDzC29hvu5AdPCp7yY5sA1u2lgokHzVKc0HkD~o8AlT4aR6cemLTTZ4IuT4rowgfHQ1Qy~bdZHPAkHdYF0GV6jrRY647XcE4yNekCbtic8E2DxMJ5qMe6-ipiegmv5qup03Dryc07IYQ0p4x0ttcrR1lug88h0beC9lPKtmV2xJ~JatKh4SonbF0DtdUtUw00oMndeHhq~plAxggRAw__" alt="">
+        </div>
+      </div>
+
+      <!--page2-->
+      <div id="page2">
+        <div id="page2text">
+        <h2>Our Experties</h2>
+        <h1>From mind to <span>spotlite,</span> idea travel <br>
+        and we make it <span>happen!</span></h1>
+
+        <p>We're not just planners; we're storytellers, coordinators, and creators. Our services <br>
+         cover every aspect of event planning, ensuring that event is a unique, <br>
+        memorable experience.</p>
+        </div>
+        
+        <div id="swiperdiv">
+              <!-- Swiper -->
+  <div class="swiper mySwiper">
+    <div class="swiper-wrapper">
+
+      <div class="swiper-slide">
+        <div class="image">
+            <img src="https://s3-alpha-sig.figma.com/img/fb96/b1ea/c460191793241005414001432c72422c?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hMcnNUfj~n~pRb5MEjZRT1-Su8wKwZ0dt71ggUneHFXLWaYUTuzaOF1KMqIWRitDvojRZtIcM5rSYifeVZw3w~E4c9js0-JCZhgNHsreM46sgsCYGHWVS4yyas6TnmAjBeDupJaVu5u1cWSN5UBkdJyFKrcJdZAoJ5tu28a-tWk5jSvLsndOEag9jE0QO1VyTqOH0QPBigswMQyIeGxdWGr~vPP92Qb~42lmg9HwkYR4otrTmcFcYFeFPCDlOIvaQuSghCZOwsej3zZzT6dbyrMKT6TkISpd4Vgffw0sM8m6t0vZno1clr2S8d1ZhbMuIw6XdoTMW~MDa2ZAjImB7w__" alt="">
+        </div>
+         <h2>Fashion Show</h2><br>
+         <p>Organization and managing <br> runway shows, designers <br> Showcase, and styling events. </p></div>
+
+      <div class="swiper-slide">
+        <div class="image">
+            <img src="https://s3-alpha-sig.figma.com/img/806c/b92a/a0a46ea14815681cbd0750e979ce78f4?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=mcV7Km~6neHhY9mhGTWn5gQERedbshLVZcSRikI7sTZNtNIhZ191wxD7IKf-xRqVv68-y4gCMOIPp8TnM2gXLbpoaaeWP1U1tUkVKfWSX8d6X23Ur5flIX9N~-S6SjiW2kGBJcp1gfqLV7zioPrT~pHEwGmUF~Cdfsp6vKdWjzFP5uSPJoWTm9PyeCjx7kGpE8KFlB5kADZrWhg~0tugw8x29JdhnE2sLMRRq-yiAGiv2LtheJW8Cp2N4jqnsdMxu6n3lVA6NFzRwf0mfVhVG9NnbyDrfhpqztBpcGOxsTPjR4~bRXj~0APAhP10-CMfNbgsN--8J-cpqfVdTaKmSA__" alt="">
+        </div> 
+        <h2>College events</h2><br>
+      <p>Handeling college festivals, <br> cultural programs, and <br> academic celebrations. </p></div>
+
+      <div class="swiper-slide">
+      <div class="image">
+            <img src="https://s3-alpha-sig.figma.com/img/1897/bf6b/1769a05f3124e7be4248fbdaef9d610b?Expires=1733702400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nWastZITRpNnjKF0WA0SD3Lf3Ow5IRl1AnLhkEU9zCTdrcBfadbyqqRJgo-cvDww7MV6G7qDk5ibArdCDaoaB3EYHUtgnohcfkRsKku4ITumr~aTxMm~mZlOoqipDW5q97usT0Mn3f4n-Ngz8Z6usy0lgMFFbVIi0rzbVN0EGJajkkaohfTaHuTgn9jXjRDQRcGqJuk5DeHKQmpTmC176etvH8ekCEIgN7MMJRma7ZQjZG1yZUrB~OjQo0KqOoujjFdDwdMV-6sAXphXMTQydZq3Mkw7lMZNtSj-oIvL44vWSHkrEb4kmaYgCN4t2IJMVCLR8xO08iXX7oG1oDZ~QA__" alt="">
+        </div>
+        <h2>Dance events</h2><br>
+      <p>Managing dance competitions, <br> performances, and themed <br> dance nights. </p></div>
+
+      <div class="swiper-slide">
+      <div class="image">
+            <img src="" alt="">
+        </div>
+        <h2>Workshops</h2><br>
+      <p>planning and facilitiating<br>educational or skill-based<br>workshops for various <br> audiance</p></div>
+      
+      <div class="swiper-slide">
+      <div class="image">
+            <img src="" alt="">
+        </div>
+        <h2>Sports events</h2><br>
+    <p>Organizing sports tournaments <br>marathons, and fitness-related <br>events. </p></div>
+
+      <div class="swiper-slide">
+      <div class="image">
+            <img src="" alt="">
+        </div>
+        <h2>Seminar</h2><br>
+      <p>Coordinating professional <br>seminars, confrences, and <br>knowledge-sharing sessions.</p></div>
+
+      <div class="swiper-slide">
+      <div class="image">
+            <img src="" alt="">
+        </div>
+        <h2>Exhibition</h2><br>
+    <p>setting up trade shows, art <br>exhibts, and product showcases.</p></div>
+      
+
     
+
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+ 
+        </div>
+      </div>
+
+    </div><!--main-->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="script.js"></script>
 </body>
 </html>
