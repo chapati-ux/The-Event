b35f9583138399d8fe47277f1d4c1836697c54ad
Sakshi <sakshivs207@gmail.com>
2024-11-27 14:03:57 +0530

    code

---
+++ about.php
@@ -0,0 +1,181 @@
+<?php include("connection.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="about.css">
+</head>
+<body>
+
+    <!-- navbar -->
+
+    <!-- first container -->
+    <div class="container1">
+        <div class="content1">
+            <div class="heading">
+                <h1>How <span class="highlight">It</span> All</span><br><span class="highlight">Started</span></h1>
+            </div>
+            <div class="description1">
+                <p>
+                Our story began with a determined college student who faced endless struggles managing registrations
+                for his campus events. From incomplete data to last-minute chaos, the process was overwhelming.
+                Frustrated by the lack of a streamlined solution, he decided to turn his problem into an opportunity.
+                With a vision to simplify event registrations and management for schools, colleges, and local events,
+                <strong>*The Happening Events*</strong> was born.
+                </p>
+            </div>
+        </div>
+    </div>
+
+     <!-- box -->
+    <div class="box">
+        <h2>BOX</h2>
+    </div>
+    
+    <!-- second container -->
+    <div class="container2">
+        <div class="content2">
+            <div class="description2">
+                <p>
+                <strong>*The Happening Events*</strong>is not just about registrations 
+                we're experts in full-scale event management too.
+                From planning and logistics to execution and post-
+                event follow-ups, our experienced team ensures every
+                detail is handled with precision. Whether it's a fashion 
+                show, workshop, sports event, or seminar, we deliver 
+                events that leave a lasting impact
+                </p>
+            </div>
+            <div class="heading">
+                <h1>More Than Just a<br><span class="highlight">Registration</span><br>Portal</h1>
+            </div>
+        </div>
+    </div>
+
+     <!-- box -->
+    <div class="box">
+        <h2>BOX</h2>
+    </div>
+
+    <!-- third container -->
+    <div class="container3">
+        <div class="content3">
+            <div class="heading">
+                <h1>Solving your<br><span class="highlight">Registration</span><br>Hassels</h1>
+            </div>
+            <div class="description3">
+                <p>
+                If you've ever faced the frustration of managing event 
+                registrations, we're here to make it effortless.
+                <strong>*The Happening Events*</strong> portal takes care of all your 
+                registration needs, ensuring smooth processes while 
+                you focus on creating memorable experiences.
+                </p>
+            </div>
+        </div>
+    </div>
+
+     <!-- box -->
+    <div class="box">
+        <h2>BOX</h2>
+    </div>
+
+    <!-- fourth container -->
+    <div class="container4">
+        <div class="content4">
+            <div class="description4">
+                <p>
+                We understand that every event is unique, which is 
+                why we offer expertly managed solutions that go 
+                beyond the ordinary. Our team of professionals uses 
+                cutting-edge technology and innovative strategies to 
+                ensure your event not only meets but exceeds expectations.
+                </p>
+            </div>
+            <div class="heading">
+                <h1>Expertly <span class="highlight">Managed</span><br>Events <span class="highlight">That</span> Exceed<br><span class="highlight">Expectations</span></h1>
+            </div>
+        </div>
+    </div>
+
+     <!-- box -->
+    <div class="box">
+        <h2>BOX</h2>
+    </div>
+
+    <!-- fifth container -->
+    <div class="container5">
+        <div class="content5">
+            <div class="heading">
+                <h1>Leave the Details<br><span class="highlight">to Us - Your</span><br>Satisfaction is<br><span class="highlight">Our Priority</span></h1>
+            </div>
+            <div class="description5">
+                <p>
+                With our user-friendly portal and event management 
+                expertise, you can concentrate on what truly matters: 
+                connecting with your audience and delivering 
+                impactful moments. We handle every intricate detail, 
+                offering you peace of mind and a guarantee of 
+                excellence.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <!-- box -->
+    <div class="box">
+        <h2>BOX</h2>
+    </div>
+
+    <!-- sixth container -->
+    <div class="container6">
+        <div class="content6">
+            <div class="description6">
+                <p>
+                We understand that every event is unique, which is 
+                why we offer expertly managed solutions that go 
+                beyond the ordinary. Our team of professionals uses 
+                cutting-edge technology and innovative strategies to 
+                ensure your event not only meets but exceeds 
+                expectations.
+                </p>
+            </div>
+            <div class="heading">
+                <h1>From a Student's<br><span class="highlight">Idea to a</span><br>Community's<br><span class="highlight">Solution</span></h1>
+            </div>
+        </div>
+    </div>
+
+     <!-- box -->
+    <div class="box">
+        <h2>BOX</h2>
+    </div>
+
+    <!-- tagline -->
+    <section class="tagline-section">
+        <div class="heading" id="heading">
+            <h1>Choose <span class="highlight">The Happening Events</span> –</h1>
+            <h1>Where Your Vision Meets Expertise!</h1>
+        </div>
+    </section>
+
+    <!-- form -->
+    <section class="form-section">
+        <div class="form-container">
+            <div class="form" action="#" method="POST">
+                <input type="text" name="question" placeholder="Do you have any questions" required>
+                <button type="submit" class="send-message">Send message</button>
+            </div>
+        </div>
+
+        <!-- contact -->
+        <div class="contact-info">
+            <p>+91 9769223537</p>
+            <p>thehappeningevents11@gmail.com</p>
+            <p>Ulwe Navi Mumbai</p>
+        </div>
+    </section>
+</body>
+</html>
